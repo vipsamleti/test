9f12dee27e38b09ad37e870c51e1519ebba2df6c
Vipin Samleti <vipin@prodevans.com>
2018-05-10 18:23:54 +0530

    changes

--- src/main/java/com/prodevans/test/test/TestApplication.java
+++ src/main/java/com/prodevans/test/test/TestApplication.java
@@ -3,6 +3,9 @@ package com.prodevans.test.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 public class TestApplication {
 
